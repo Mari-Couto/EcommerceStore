333209186c958e1dc9a48e29e21f0c0526053e6c
Mari-Couto <marinacoutonina@gmail.com>
2024-05-10 15:03:57 -0300

    post de pedidosItens funcionando

--- routes/pedidosItens.js
+++ routes/pedidosItens.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const mysql = require('../mysql')
 
 router.get('/', (req, res) => {
     res.status(200).send({
@@ -7,17 +8,30 @@ router.get('/', (req, res) => {
     });
 });
 
+//Inserir item do pedido
 router.post('/', (req, res) => {
-    const pedidosItens = {
-        quantidade: req.body.quantidade,
-        preco_unitario: req.body.preco_unitario,
-      };
-      res.status(201).send({
-        mensagem: "Insere um item do pedido",
-        produtoCriado: pedidosItens
-      })
+    const { IdPedido, IdProduto, Quantidade, Preco_unitario } = req.body;
+    if (!IdProduto || !Quantidade || !Preco_unitario) {
+        return res.status(400).json({ error: 'Todos os campos são obrigatórios:' });
+    }
+    try {
+        mysql.query(
+            'INSERT INTO pedidosItens (IdPedido, IdProduto, Quantidade, Preco_unitario) VALUES (?, ?, ?, ?)',
+            [IdPedido, IdProduto, Quantidade, Preco_unitario],
+            (err, result) => {
+                if (err) {
+                    console.error('Erro ao inserir item de pedido:', err);
+                    return res.status(500).json({ error: 'Erro ao inserir item de pedido' });
+                }
+                res.status(201).json({ message: 'Item de pedido inserido com sucesso', IdPedidoItem: result.insertId });
+            }
+        );
+    } catch (error) {
+        console.error('Erro ao processar a requisição:', error);
+        res.status(500).json({ error: 'Erro interno ao processar a requisição' });
+    }
 });
-
+  
 router.patch('/', (req, res) => {
     res.status(202).send({
         mensagem: 'Patch funcionando em rota de itens do pedido'
