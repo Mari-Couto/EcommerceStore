f035089e1c8c11fdfd459e070d12262a1aa20eca
Mari-Couto <marinacoutonina@gmail.com>
2024-05-13 11:17:32 -0300

    conexão com o banco

--- mysql.js
+++ mysql.js
@@ -1,20 +1,25 @@
 const password = process.env.MYSQL_PASSWORD;
 const mysql = require('mysql2')
 
-const connection = mysql.createConnection({
+
+const pool = mysql.createPool({
     "user": "root",
     "password": password,
     "database": "ecommerce",
     "host": "localhost",
-    "port": 3306
+    "port": 3306,
+    "connectionLimit": 10, 
+    "waitForConnections": true, 
+    "queueLimit": 0 
 });
 
-connection.connect((err) => {
-    if(err) {
-        console.error("Erro ao conectar ao banco de dados:", err)
+pool.getConnection((err, connection) => {
+    if (err) {
+        console.error("Erro ao conectar ao banco de dados:", err);
         return;
     }
-    console.log("Conexão bem-sucedida ao banco de dados")
-})
+    console.log("Conexão bem-sucedida ao banco de dados");
+    connection.release(); 
+});
 
-module.exports = connection;
+module.exports = pool;
