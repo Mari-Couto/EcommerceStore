eb3df84acd4cfd6706f3acc4dfc801211d6476ca
Mari-Couto <marinacoutonina@gmail.com>
2024-05-15 15:27:18 -0300

    navbar pronto

--- EcommerceFront/src/App.jsx
+++ EcommerceFront/src/App.jsx
@@ -1,10 +1,11 @@
+import Navbar from '../components/Navbar'
 import './App.css'
 
 function App() {
   return (
   
       <div>
-        <h1>Iniciando Front End com React</h1>
+        <Navbar/>
     </div>
   )
 }
