a6e8a59af21900e46fa34801ac42b3fedc02c3e0
Mari-Couto <marinacoutonina@gmail.com>
2024-05-20 15:24:14 -0300

    post de pedidos com itens

--- EcommerceFront/src/components/PostOrders.jsx
+++ EcommerceFront/src/components/PostOrders.jsx
@@ -2,12 +2,22 @@ import axios from 'axios';
 
 const postOrder = async (product) => {
   try {
-    const response = await axios.post('http://localhost:3000/pedidos', {
+    const orderResponse = await axios.post('http://localhost:3000/pedidos', {
       produtoId: product.idProduto,
-      quantidade: 1, 
+      quantidade: 1,
       preco: product.precoProduto,
     });
-    return response.data;
+
+    const orderId = orderResponse.data.IdPedido;
+
+    await axios.post('http://localhost:3000/pedidosItens', {
+      IdPedido: orderId,
+      IdProduto: product.idProduto,
+      Quantidade: 1,
+      Preco: product.precoProduto,
+    });
+
+    return orderResponse.data;
   } catch (error) {
     console.error('Erro ao inserir pedido:', error);
     throw error;
