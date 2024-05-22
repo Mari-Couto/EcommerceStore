c1d2fcd09ccb3e8b97646f14fc0b28ce67bbf726
Mari-Couto <marinacoutonina@gmail.com>
2024-05-22 16:57:15 -0300

    patch de categoria

--- EcommerceFront/src/components/CategoryCard.jsx
+++ EcommerceFront/src/components/CategoryCard.jsx
@@ -1,7 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
+import axios from 'axios';
 import './Category.css';
 
 const CategoryCard = ({ category }) => {
+  const [newName, setNewName] = useState(category.nomeCategoria);
+  const [isEditing, setIsEditing] = useState(false);
+
+  const handleInputChange = (e) => {
+    setNewName(e.target.value);
+  };
+
+  const handleUpdate = async () => {
+    try {
+      await axios.patch(`http://localhost:3000/categoria/${category.IdCategoria}`, { nomeCategoria: newName });
+      setIsEditing(false);
+    } catch (error) {
+      console.error('Erro ao atualizar a categoria:', error);
+    }
+  };
+
   return (
     <div className="category-card">
       <div className="category-details">
@@ -16,10 +33,24 @@ const CategoryCard = ({ category }) => {
           <tbody>
             <tr>
               <td className='categoriesNames'>{category.IdCategoria}</td>
-              <td className='categoriesNames'>{category.nomeCategoria}</td>
+              <td className='categoriesNames'>
+                {isEditing ? (
+                  <input
+                    type="text"
+                    value={newName}
+                    onChange={handleInputChange}
+                  />
+                ) : (
+                  category.nomeCategoria
+                )}
+              </td>
               <td>
                 <div className='button-container'>
-                  <button className="edit-button">Editar</button>
+                  {isEditing ? (
+                    <button className="save-button" onClick={handleUpdate}>Salvar</button>
+                  ) : (
+                    <button className="edit-button" onClick={() => setIsEditing(true)}>Editar</button>
+                  )}
                   <button className="delete-button">Excluir</button>
                 </div>
               </td>
