f7f035a5833583d4133630c48f43ce60e191d292
Mari-Couto <marinacoutonina@gmail.com>
2024-05-23 16:59:32 -0300

    paginação do producthome

--- EcommerceFront/src/components/ProductsHome.jsx
+++ EcommerceFront/src/components/ProductsHome.jsx
@@ -1,13 +1,15 @@
 import React, { useState, useEffect } from 'react';
-import axios from 'axios'; 
-import postOrder from './PostOrders'; 
+import axios from 'axios';
+import postOrder from './PostOrders';
 import './ProductsHome.css';
 
 const ProductsHome = () => {
   const [products, setProducts] = useState([]);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
-  const [orderStatus, setOrderStatus] = useState(null); 
+  const [currentPage, setCurrentPage] = useState(1);
+  const [productsPerPage] = useState(5); 
+  const [orderStatus, setOrderStatus] = useState(null);
 
   useEffect(() => {
     async function fetchProducts() {
@@ -24,11 +26,11 @@ const ProductsHome = () => {
           };
         }));
         setProducts(productsWithImages);
-        setLoading(false); 
+        setLoading(false);
       } catch (error) {
         console.error('Erro ao obter os produtos:', error);
-        setError(error); 
-        setLoading(false); 
+        setError(error);
+        setLoading(false);
       }
     }
     fetchProducts();
@@ -42,6 +44,11 @@ const ProductsHome = () => {
       setOrderStatus('Erro ao inserir pedido. Tente novamente.');
     }
   };
+  const indexOfLastProduct = currentPage * productsPerPage;
+  const indexOfFirstProduct = indexOfLastProduct - productsPerPage;
+  const currentProducts = products.slice(indexOfFirstProduct, indexOfLastProduct);
+
+  const paginate = pageNumber => setCurrentPage(pageNumber);
 
   if (loading) {
     return <div>Carregando...</div>;
@@ -51,10 +58,10 @@ const ProductsHome = () => {
     return <div>Erro: {error.message}</div>;
   }
 
-
   return (
-    <div className="catalog">
-      {products.map(product => (
+    <div>
+      <div className="catalog">
+      {currentProducts.map(product => (
         <div key={product.idProduto} className="card-product">
           <div className="image-container">
             {product.imageUrl && <img src={product.imageUrl} alt={product.nome} className="image" />}
@@ -64,11 +71,18 @@ const ProductsHome = () => {
             <p>Preço: R$ {product.precoProduto}</p>
             <p>{product.descricao}</p>
             <button className="buy-button" onClick={() => handleBuyButtonClick(product)}>Comprar</button>
-      {orderStatus && <div className="order-status">{orderStatus}</div>}
-  
+            {orderStatus && <div className="order-status">{orderStatus}</div>}
           </div>
         </div>
       ))}
+    </div>
+      <div className="pagination">
+      {Array.from({ length: Math.ceil(products.length / productsPerPage) }).map((_, index) => (
+        <button key={index + 1} onClick={() => paginate(index + 1)} className={`page-link ${currentPage === index + 1 ? 'active' : ''}`}>
+          {index + 1}
+        </button>
+      ))}
+    </div>
     </div>
   );
 };
