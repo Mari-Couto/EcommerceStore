0a47e08180a546113488db8ef9953cba12673368
Mari-Couto <marinacoutonina@gmail.com>
2024-05-23 16:41:10 -0300

    botões de paginação do produtoscard

--- EcommerceFront/src/components/ProductCatalog.jsx
+++ EcommerceFront/src/components/ProductCatalog.jsx
@@ -3,10 +3,28 @@ import axios from 'axios';
 import ProductCard from './ProductCard';
 import './ProductCatalog.css';
 
+const Pagination = ({ totalPages, currentPage, onPageChange }) => {
+  return (
+    <div className="pagination">
+      {Array.from({ length: totalPages }, (_, index) => (
+        <button
+          key={index}
+          onClick={() => onPageChange(index + 1)}
+          className={currentPage === index + 1 ? 'active' : ''}
+        >
+          {index + 1}
+        </button>
+      ))}
+    </div>
+  );
+};
+
 const ProductCatalog = () => {
   const [products, setProducts] = useState([]);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
+  const [currentPage, setCurrentPage] = useState(1);
+  const productsPerPage = 5;
 
   useEffect(() => {
     async function fetchProducts() {
@@ -18,7 +36,6 @@ const ProductCatalog = () => {
             if (product.file) {
               imageUrl = `http://localhost:3000/produtos/imagem/${product.idProduto}`;
             }
-
             return {
               ...product,
               imageUrl: imageUrl
@@ -39,6 +56,10 @@ const ProductCatalog = () => {
     fetchProducts();
   }, []);
 
+  const handleDelete = (productId) => {
+    setProducts(prevProducts => prevProducts.filter(product => product.idProduto !== productId));
+  };
+
   if (loading) {
     return <div>Carregando...</div>;
   }
@@ -47,14 +68,23 @@ const ProductCatalog = () => {
     return <div>Erro: {error.message}</div>;
   }
 
+  const indexOfLastProduct = currentPage * productsPerPage;
+  const indexOfFirstProduct = indexOfLastProduct - productsPerPage;
+  const currentProducts = products.slice(indexOfFirstProduct, indexOfLastProduct);
+  const totalPages = Math.ceil(products.length / productsPerPage);
+
   return (
     <div className="product-catalog">
-      {products.map(product => (
-        <ProductCard key={product.idProduto} product={product} />
+      {currentProducts.map(product => (
+        <ProductCard key={product.idProduto} product={product} onDelete={handleDelete} />
       ))}
+      <Pagination
+        totalPages={totalPages}
+        currentPage={currentPage}
+        onPageChange={setCurrentPage}
+      />
     </div>
   );
-  
 };
 
 export default ProductCatalog;
