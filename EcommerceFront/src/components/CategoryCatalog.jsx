e097f5e63ce98cc5a00bd8f5b6b6bc0bc93bb092
Mari-Couto <marinacoutonina@gmail.com>
2024-05-24 10:33:34 -0300

    paginação em categorias

--- EcommerceFront/src/components/CategoryCatalog.jsx
+++ EcommerceFront/src/components/CategoryCatalog.jsx
@@ -1,22 +1,41 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import CategoryCard from './CategoryCard';
+import './ProductCatalog.css';
+
+const Pagination = ({ totalPages, currentPage, onPageChange }) => {
+  return (
+    <div className="pagination">
+      {Array.from({ length: totalPages }, (_, index) => (
+        <button
+          key={index}
+          onClick={() => onPageChange(index + 1)}
+          className={currentPage === index + 1 ? 'active' : ''}
+        >
+          {index + 1}
+        </button>
+      ))}
+    </div>
+  );
+};
 
 const CategoryCatalog = () => {
-  const [categories, setCategories] = useState([]); 
-  const [isLoading, setIsLoading] = useState(true); 
+  const [categories, setCategories] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
   const [fetchError, setFetchError] = useState(null);
+  const [currentPage, setCurrentPage] = useState(1);
+  const categoriesPerPage = 5;
 
   useEffect(() => {
     const fetchCategories = async () => {
       try {
-        const response = await axios.get('http://localhost:3000/categoria'); 
+        const response = await axios.get('http://localhost:3000/categoria');
         setCategories(response.data);
       } catch (error) {
         console.error('Error fetching categories:', error);
         setFetchError(error);
       } finally {
-        setIsLoading(false); 
+        setIsLoading(false);
       }
     };
 
@@ -31,12 +50,24 @@ const CategoryCatalog = () => {
     return <div>Error: {fetchError.message}</div>;
   }
 
+  const indexOfLastCategory = currentPage * categoriesPerPage;
+  const indexOfFirstCategory = indexOfLastCategory - categoriesPerPage;
+  const currentCategories = categories.slice(indexOfFirstCategory, indexOfLastCategory);
+  const totalPages = Math.ceil(categories.length / categoriesPerPage);
+
   return (
-    <div className="category-list">
-    {categories.map(category => (
-      <CategoryCard key={category.IdCategoria} category={category} />
-    ))}
-  </div>
+   <div>
+     <div className="category-list">
+      {currentCategories.map(category => (
+        <CategoryCard key={category.IdCategoria} category={category} />
+      ))}
+    </div>
+       <Pagination
+       totalPages={totalPages}
+       currentPage={currentPage}
+       onPageChange={setCurrentPage}
+     />
+   </div>
   );
 };
 
